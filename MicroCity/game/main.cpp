89a41a0d7bb59080308b66d9dab368545bdec5d1
apfxtech <aperturefoxtechnology@gmail.com>
2026-03-04 18:00:04 +0300

    First build

--- MicroCity/game/main.cpp
+++ MicroCity/game/main.cpp
@@ -1,20 +1,87 @@
-#include <Arduboy2.h>
-#include <EEPROM.h>
+#include "lib/Arduboy2.h"
+#include "lib/EEPROM.h"
+#include "lib/runtime.h"
+
+#undef INPUT_LEFT
+#undef INPUT_RIGHT
+#undef INPUT_UP
+#undef INPUT_DOWN
+#undef INPUT_A
+#undef INPUT_B
+
 #include "Draw.h"
 #include "Interface.h"
 #include "Game.h"
 #include "Simulation.h"
 
-Arduboy2Base arduboy;
+static constexpr uint16_t EEPROM_STORAGE_SPACE_START = 0;
+static constexpr uint32_t RETURN_HOLD_TIME_MS = 500;
+
+static uint32_t return_hold_start_ms = 0;
+static bool return_was_pressed = false;
+static bool return_hold_handled = false;
+static bool pending_return_press = false;
+
+static bool IsInMainMenuState()
+{
+  return UIState.state == StartScreen || UIState.state == NewCityMenu;
+}
+
+static void HandleReturnHold()
+{
+  if(IsInMainMenuState())
+  {
+    arduboy.exitToBootloader();
+  }
+  else
+  {
+    UIState.state = StartScreen;
+    UIState.selection = 0;
+  }
+}
+
+static void UpdateReturnButton()
+{
+  const bool is_pressed = arduboy.pressed(A_BUTTON);
+
+  if(is_pressed)
+  {
+    if(!return_was_pressed)
+    {
+      return_was_pressed = true;
+      return_hold_handled = false;
+      return_hold_start_ms = millis();
+    }
+    else if(!return_hold_handled && (uint32_t)(millis() - return_hold_start_ms) >= RETURN_HOLD_TIME_MS)
+    {
+      return_hold_handled = true;
+      pending_return_press = false;
+      HandleReturnHold();
+    }
+  }
+  else if(return_was_pressed)
+  {
+    if(!return_hold_handled)
+    {
+      pending_return_press = true;
+    }
+
+    return_was_pressed = false;
+    return_hold_handled = false;
+    return_hold_start_ms = 0;
+  }
+}
 
 uint8_t GetInput()
 {
   uint8_t result = 0;
-  
-  if(arduboy.pressed(A_BUTTON))
+
+  if(pending_return_press)
   {
-    result |= INPUT_A;  
+    result |= INPUT_A;
+    pending_return_press = false;
   }
+  
   if(arduboy.pressed(B_BUTTON))
   {
     result |= INPUT_B;  
@@ -58,7 +125,7 @@ void DrawRect(uint8_t x, uint8_t y, uint8_t w, uint8_t h, uint8_t colour)
 
 void DrawBitmap(const uint8_t* bmp, uint8_t x, uint8_t y, uint8_t w, uint8_t h)
 {
-  arduboy.drawBitmap(x, y, bmp, w, h, WHITE);
+  arduboy.drawBitmap(x, y, bmp, w, h, BLACK);
 }
 
 void SaveCity()
@@ -77,6 +144,7 @@ void SaveCity()
     EEPROM.update(address++, *ptr);
     ptr++;
   }
+  EEPROM.commit();
 }
 
 bool LoadCity()
@@ -106,8 +174,6 @@ uint8_t* GetPowerGrid()
 void setup()
 {
   arduboy.boot();
-  arduboy.flashlight();
-  arduboy.systemButtons();
   arduboy.bootLogo();
   arduboy.setFrameRate(25);
 
@@ -118,8 +184,9 @@ void loop()
 {
   if(arduboy.nextFrame())
   {
+    arduboy.pollButtons();
+    UpdateReturnButton();
     TickGame();
     arduboy.display(false);
   }
 }
-
--- MicroCity/readme.md
+++ MicroCity/readme.md
@@ -1,13 +1,37 @@
 # MicroCity
 
-![Demo](Images/demo.gif)
+**A city-building simulation game for Arduboy inspired by SimCity**
 
-MicroCity is a city simulation game for the [Arduboy miniature game system](https://www.arduboy.com) inspired by SimCity
+![Demo](package/Images/demo.gif)
 
-## Build instructions
-To compile from source you will need the [Arduboy2 library](https://github.com/MLXXXp/Arduboy2) and the Arduino IDE
+## Features
 
-Open /Source/MicroCity/MicroCity.ino in the Arduino IDE and hit build
+### City Building
+- **Zone different areas**: Residential, Commercial, and Industrial
+- **Build infrastructure**: Roads and power lines to connect your city
+- **Essential services**: Police stations and fire departments
+- **Special buildings**: Parks, stadiums, and power plants
+- **48×48 tile map** with scrollable viewport
 
-## Flashing other games
-Note that there is a bug with the Arduboy bootloader when flashing new games. If flashing a new Arduino sketch after having previously flashing MicroCity, then first boot the Arduboy into *flashlight mode* by holding the up button whilst switching on the device.
+### Simulation
+- **Dynamic population growth** based on employment, taxes, and quality of life
+- **Crime system** affected by police coverage
+- **Fire disasters** that can spread to nearby buildings
+- **Pollution mechanics** from industry and traffic
+- **Power grid management** - buildings need electricity to thrive
+
+### Economy
+- **Starting funds**: $10,000
+- **Tax collection** based on population and tax rate
+- **Budget management** for roads, police, and fire services
+
+## Screenshots
+| | |
+|---|---|
+| ![s1](package/screenshots/1.png) | ![s2](package/screenshots/2.png) |
+| ![s3](package/screenshots/3.png) | ![s4](package/screenshots/4.png) |
+
+## Original (Arduboy)
+**James Howard**
+[MicroCity](https://github.com/jhhoward/MicroCity)
+
